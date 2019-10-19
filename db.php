ae7a07c23517139d416266165fd42329077a99d0
aksharma4 <50731961+aksharma4@users.noreply.github.com>
2019-10-19 17:22:10 +0530

    first

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+define('HOSTNAME','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME', 'chatbot');
+
+//global $con;
+$conn = mysqli_connect(HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME) ;
+
+
+?>
